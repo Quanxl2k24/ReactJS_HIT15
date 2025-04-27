5cae77f396f2d7d26e96c03f805d8690fb67920c
Quanxl2k24 <quanxl2k24@gamil.com>
2025-04-27 13:04:25 +0700

    tx1

--- vite-project_tx1/src/components/Management/Management.jsx
+++ vite-project_tx1/src/components/Management/Management.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import "./Management.scss";
+
 export default function Management() {
+  const [index, setindex] = useState(null);
   const [editButton, setEditButton] = useState(false);
   const [display, setDisplay] = useState(false);
   const [id, setId] = useState("");
   const [ten, setTen] = useState("");
   const [diadiem, setDiadiem] = useState("");
   const [mota, setMota] = useState("");
-  const [data, setData] = useState([
-    { id: "", ten: "", diadiem: "", mota: "" },
-  ]);
+  const [data, setData] = useState([]);
 
   const handleclickadd = () => {
     setDisplay(true);
@@ -21,29 +21,49 @@ export default function Management() {
       { id: id, ten: ten, diadiem: diadiem, mota: mota },
     ]);
     setDisplay(false);
+    resetInput();
   };
 
-  const handleupdate = (index) => {
-    setDisplay(true);
+  const handleupdate = () => {
+    const newData = [...data];
+    newData[index] = { id, ten, diadiem, mota };
+    setData(newData);
+    setDisplay(false);
+    setEditButton(false);
+    resetInput();
+  };
+
+  const handleDel = (index) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
+  };
+
+  const resetInput = () => {
+    setId("");
+    setTen("");
+    setDiadiem("");
+    setMota("");
+  };
+
+  const handle = () => {
+    if (editButton) {
+      handleupdate();
+    } else {
+      handleaddinfo();
+    }
+  };
+
+  const handleupdateClick = (index) => {
+    setindex(index);
     setId(data[index].id);
     setTen(data[index].ten);
     setDiadiem(data[index].diadiem);
     setMota(data[index].mota);
     setEditButton(true);
-    console.log("🚀 ~ handleupdate ~ data[index]:", data[index]);
+    setDisplay(true);
   };
 
-  const handleDel = (index) => {
-    const newdata = [...data];
-    newdata.splice(index, 1);
-    setData(newdata);
-  };
-
-   const handle =()=>{
-    if(editButton){
-        
-    }
-   }
   return (
     <div className="Management-container">
       {display && (
@@ -74,12 +94,13 @@ export default function Management() {
             onChange={(e) => setMota(e.target.value)}
           />
           <div className="input-data_butadd">
-            <button className="butadd" onClick={handleaddinfo}>
+            <button className="butadd" onClick={handle}>
               {editButton ? "Sua" : "Them"}
             </button>
           </div>
         </div>
       )}
+
       <div className="management">
         <div className="management_header">
           <div className="management_header--title">
@@ -95,28 +116,33 @@ export default function Management() {
 
         <div className="management_box">
           <table>
-            <tr>
-              <th>ID</th>
-              <th>TEN SU KIEN</th>
-              <th>DIA DIEM</th>
-              <th>MO TA</th>
-              <th>ACTOR</th>
-            </tr>
-
-            {data.map((e, index) => (
-              <tr key={e.id}>
-                <td>{e.id}</td>
-                <td>{e.ten}</td>
-                <td>{e.diadiem}</td>
-                <td>{e.mota}</td>
-                <td>
-                  <div className="button-del-and-upd">
-                    <button onClick={() => handleupdate(index)}>Sua</button>
-                    <button onClick={() => handleDel(index)}>Xoa</button>
-                  </div>
-                </td>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>TEN SU KIEN</th>
+                <th>DIA DIEM</th>
+                <th>MO TA</th>
+                <th>ACTOR</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {data.map((e, index) => (
+                <tr key={e.id}>
+                  <td>{e.id}</td>
+                  <td>{e.ten}</td>
+                  <td>{e.diadiem}</td>
+                  <td>{e.mota}</td>
+                  <td>
+                    <div className="button-del-and-upd">
+                      <button onClick={() => handleupdateClick(index)}>
+                        Sua
+                      </button>
+                      <button onClick={() => handleDel(index)}>Xoa</button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
